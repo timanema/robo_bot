c9abad73e05cf896528592b7123895f37b38a8bc
Tim Anema <tim.anema@hotmail.nl>
2020-01-20 13:50:58 +0100

    yes i can git

--- robot_sub/robot_sub.ino
+++ robot_sub/robot_sub.ino
@@ -2,7 +2,7 @@
 #include <geometry_msgs/Twist.h>
 
 #define PIN_LED 13
-#define PIN_DIST_TRIG 23
+#define PIN_DIST_TRIG 23  
 #define PIN_DIST_ECHO 22
 #define PIN_MOTOR1_EN 24
 #define PIN_MOTOR1_FWD 6
@@ -58,25 +58,28 @@ void loop()
     analogWrite(PIN_MOTOR1_REV, 0);
     analogWrite(PIN_MOTOR2_REV, 0);
 
-    int left = 0;
-    int right = 0;
+    float left = 0;
+    float right = 0;
     
     if (direction < 0) {
       left = 0;
       right = 0;
     } else if (direction < 90) {
       left = 255;
-      right = int(float(direction) / 90 * 255);
+      right = float(direction) / 90 * 255;
     } else if (direction > 90) {
-      left = 255 - int((float(direction) - 90) / 90 * 255);
+      left = 255 - (float(direction) - 90) / 90 * 255;
       right = 255;
     } else {
       left = 255;
       right = 255;
     }
     
-    analogWrite(PIN_MOTOR1_FWD, left);
-    analogWrite(PIN_MOTOR2_FWD, right);
+    left *= 0.3;
+    right *= 0.3;
+    
+    analogWrite(PIN_MOTOR1_FWD, int(left));
+    analogWrite(PIN_MOTOR2_FWD, int(right));
     
   } else if(distance <= 5) {
     // Backwards!
@@ -86,14 +89,11 @@ void loop()
     analogWrite(PIN_MOTOR2_REV, 255);
   } else {
     // Stop!
+    digitalWrite(PIN_MOTOR1_EN, LOW);
+    digitalWrite(PIN_MOTOR2_EN, LOW);
     analogWrite(PIN_MOTOR1_FWD, 0);
-    analogWrite(PIN_MOTOR1_REV, 255);
-    analogWrite(PIN_MOTOR2_FWD, 0);
-    analogWrite(PIN_MOTOR2_REV, 255);
-    
-    delay(5);
-    
     analogWrite(PIN_MOTOR1_REV, 0);
+    analogWrite(PIN_MOTOR2_FWD, 0);
     analogWrite(PIN_MOTOR2_REV, 0);
   }
 }
